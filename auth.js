10fd2960eebfc65bce16114f192d3cd38d76c4fe
Kehali Tadesse <kehalitadesse707@gmail.com>
2026-03-19 21:26:46 +0300

    CORS

--- auth.js
+++ auth.js
@@ -1,5 +1,12 @@
-module.exports = (app) => {
+const auth = require('better-auth')
+
+module.exports = (app) => {   
     app.post('/api/signup', (req, res) => {
         console.log(req.body)
     })
+
+    app.post('/api/signin', (req, res) => {
+        console.log(req.body)
+    })
+
 }
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
 
-app.use(cors())
+app.use(cors({
+    origin: process.env.FRONTEND_URL
+}))
+
 app.use(express.json())
+
 
 require('./auth')(app)
 
