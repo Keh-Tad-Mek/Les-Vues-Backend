7ff0461f01d1188df0c7cdd636955270a768cacb
Keh-Tad-Mek <kehalitadesse707@gmail.com>
2026-08-27 09:26:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import { movieSearchRoutes } from './movies/search.js';
 import { seriesRoutes } from './movies/seriesRoutes.js';
 import { favoritesRoute } from './personal/favorites.js';
 import { saveForLaterRoute } from './personal/saveForLater.js';
-import { health } from './personal/favorites.js.;
+import { health } from './personal/favorites.js';
 
 const app = express();
 
